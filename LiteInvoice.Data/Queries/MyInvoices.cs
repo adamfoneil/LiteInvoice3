430b8aeec51f4443e48d9092b8c9f9f82e4ac675
Adam O'Neil <adamosoftware@gmail.com>
2024-02-04 10:56:39 -0500

    started on invoices page

---
+++ LiteInvoice.Data/Queries/MyInvoices.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.QX.Abstract;
+using Dapper.QX.Interfaces;
+using LiteInvoice.Data.Entities;
+using LiteInvoice.Data.Interfaces;
+
+namespace LiteInvoice.Data.Queries;
+
+public class MyInvoices : TestableQuery<Invoice>, IUserQuery
+{
+	public MyInvoices() : base(
+		"""
+
+		""")
+	{		
+	}
+
+	public int UserId { get; set; }
+
+	protected override IEnumerable<ITestableQuery> GetTestCasesInner()
+	{
+		throw new NotImplementedException();
+	}
+}
