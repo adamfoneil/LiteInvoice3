a64a956461742d886f36329867c4d86bebc3cc77
Adam O'Neil <adamosoftware@gmail.com>
2024-02-11 08:23:35 -0500

    new wasm project

---
+++ WebApp/WebApp/LinkedConfigFile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace WebApp;
+
+public class LinkedConfigFile : IFileProvider
+{
+	public IDirectoryContents GetDirectoryContents(string subpath)
+	{
+		throw new NotImplementedException();
+	}
+
+	public IFileInfo GetFileInfo(string subpath)
+	{
+		var allJsonFiles = Directory.GetFiles(Environment.CurrentDirectory, "*.json", SearchOption.AllDirectories);
+		var fullPath = allJsonFiles.SingleOrDefault(path => path.EndsWith(subpath)) ?? throw new FileNotFoundException($"File not found: {subpath}");		
+		throw new NotImplementedException();
+	}
+
+	public IChangeToken Watch(string filter)
+	{
+		throw new NotImplementedException();
+	}
+}
